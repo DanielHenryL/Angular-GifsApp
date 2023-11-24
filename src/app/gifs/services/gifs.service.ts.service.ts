a4f9521cbdaf2aceee42aced8968ffb1064bd185
DanielHenryL <lagunasdaniel001@gmail.com>
2023-11-24 16:31:27 -0500

    Validaciones al servicio

--- src/app/gifs/services/gifs.service.ts.service.ts
+++ src/app/gifs/services/gifs.service.ts.service.ts
@@ -12,8 +12,28 @@ export class GifsService {
   get TagsHistory(){
     return [...this._tagsHistory];
   }
-  searchTag( tag:string ):void {
+  private lowerCaseTag(tag:string){
+    tag = tag.toLowerCase();
+  }
+  private existTagAndRemove(tag:string){
+    const existTag = this._tagsHistory.includes(tag);
+    if( existTag ){
+      this._tagsHistory = this._tagsHistory.filter( oldTag => oldTag !== tag)
+    }
+  }
+
+  private organizeHistory( tag: string ){
+    this.lowerCaseTag(tag);
+    this.existTagAndRemove(tag);
     this._tagsHistory.unshift( tag );
+    this._tagsHistory = this._tagsHistory.splice(0,20);
+  }
+
+  searchTag( tag:string ):void {
+    if( tag.length === 0 ) return;
+
+    this.organizeHistory( tag );
+
   }
 
 }
